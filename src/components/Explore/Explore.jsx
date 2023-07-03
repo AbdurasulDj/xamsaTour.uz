17bb10d6e194d8a956dd535e31a74c1c0d2faece
AbdurasulDj <abdurasulac@gmail.com>
2023-07-03 17:49:26 +0500

    added tours mapping and changed home h1 font-size

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import ToursData from "./assets/Tours.json";
+
+import Tour from "./assets/Tour";
 
 import "./Explore.css";
 
-function Explore(props) {
+function Explore() {
   const { t } = useTranslation();
+  const [tours, setTours] = useState(() => ToursData);
 
   return (
     <div id="agency">
@@ -23,156 +27,10 @@ function Explore(props) {
             </h1>
           </div>
           <div className="row g-4">
-            <div
-              className="col-lg-4 col-md-6 wow fadeInUp"
-              data-wow-delay="0.1s"
-            >
-              <div className="room-item shadow rounded overflow-hidden">
-                <div className="position-relative">
-                  <img
-                    className="img-fluid"
-                    src="./Samarqand.jpg"
-                    alt=""
-                  />
-                  <small className="position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4">
-                    $25/{t(`day`)}
-                  </small>
-                </div>
-                <div className="p-4 mt-2">
-                  <div className="d-flex justify-content-between mb-3">
-                    <h5 className="mb-0">{t(`samarqand`)}</h5>
-                    <div className="ps-2">
-                      <small className="fa fa-star text-primary"></small>
-                      <small className="fa fa-star text-primary"></small>
-                      <small className="fa fa-star text-primary"></small>
-                      <small className="fa fa-star text-primary"></small>
-                      <small className="fa fa-star text-primary"></small>
-                    </div>
-                  </div>
-                  <div className="d-flex mb-3">
-                    <small className="border-end me-3 pe-3">
-                      <i className="fa fa-bed text-primary me-2"></i>
-                      {t(`room`)}
-                    </small>
-                    <small className="border-end me-3 pe-3">
-                      <i className="fa fa-utensils text-primary me-2"></i>
-                      {t(`food`)}
-                    </small>
-                    <small>
-                      <i className="fa fa-headset text-primary me-2"></i>
-                      {t(`gid`)}
-                    </small>
-                  </div>
-                  <p className="text-body mb-3">
-                    {t(`samarqand_p`)}
-                  </p>
-                  {/* <div className="d-flex justify-content-between">
-                                    <a className="btn btn-sm btn-primary rounded py-2 px-4" href="">View Detail</a>
-                                    <a className="btn btn-sm btn-dark rounded py-2 px-4" href="">Book Now</a>
-                                </div> */}
-                </div>
-              </div>
-            </div>
-            <div
-              className="col-lg-4 col-md-6 wow fadeInUp"
-              data-wow-delay="0.3s"
-            >
-              <div className="room-item shadow rounded overflow-hidden">
-                <div className="position-relative">
-                  <img
-                    className="img-fluid"
-                    src="./turkia.jpg"
-                    alt=""
-                  />
-                  <small className="position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4">
-                    $50/{t(`day`)}
-                  </small>
-                </div>
-                <div className="p-4 mt-2">
-                  <div className="d-flex justify-content-between mb-3">
-                    <h5 className="mb-0">{t(`turkiya`)}</h5>
-                    <div className="ps-2">
-                      <small className="fa fa-star text-primary"></small>
-                      <small className="fa fa-star text-primary"></small>
-                      <small className="fa fa-star text-primary"></small>
-                      <small className="fa fa-star text-primary"></small>
-                      <small className="fa fa-star text-primary"></small>
-                    </div>
-                  </div>
-                  <div className="d-flex mb-3">
-                    <small className="border-end me-3 pe-3">
-                      <i className="fa fa-bed text-primary me-2"></i>
-                      {t(`room`)}
-                    </small>
-                    <small className="border-end me-3 pe-3">
-                      <i className="fa fa-utensils text-primary me-2"></i>
-                      {t(`food`)}
-                    </small>
-                    <small>
-                      <i className="fa fa-headset text-primary me-2"></i>
-                      {t(`gid`)}
-                    </small>
-                  </div>
-                  <p className="text-body mb-3">
-                    {t(`turkiya_p`)}
-                  </p>
-                  {/* <div className="d-flex justify-content-between">
-                                    <a className="btn btn-sm btn-primary rounded py-2 px-4" href="">View Detail</a>
-                                    <a className="btn btn-sm btn-dark rounded py-2 px-4" href="">Book Now</a>
-                                </div> */}
-                </div>
-              </div>
-            </div>
-            <div
-              className="col-lg-4 col-md-6 wow fadeInUp"
-              data-wow-delay="0.6s"
-            >
-              <div className="room-item shadow rounded overflow-hidden">
-                <div className="position-relative">
-                  <img
-                    className="img-fluid"
-                    src="./dubai.jpg"
-                    alt=""
-                  />
-                  <small className="position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4">
-                    $100/{t(`day`)}
-                  </small>
-                </div>
-                <div className="p-4 mt-2">
-                  <div className="d-flex justify-content-between mb-3">
-                    <h5 className="mb-0">{t(`dubai`)}</h5>
-                    <div className="ps-2">
-                      <small className="fa fa-star text-primary"></small>
-                      <small className="fa fa-star text-primary"></small>
-                      <small className="fa fa-star text-primary"></small>
-                      <small className="fa fa-star text-primary"></small>
-                      <small className="fa fa-star text-primary"></small>
-                    </div>
-                  </div>
-                  <div className="d-flex mb-3">
-                    <small className="border-end me-3 pe-3">
-                      <i className="fa fa-bed text-primary me-2"></i>
-                      {t(`room`)}
-                    </small>
-                    <small className="border-end me-3 pe-3">
-                      <i className="fa fa-utensils text-primary me-2"></i>
-                      {t(`food`)}
-                    </small>
-                    <small>
-                      <i className="fa fa-headset text-primary me-2"></i>
-                      {t(`gid`)}
-                    </small>
-                  </div>
-                  <p className="text-body mb-3">
-                  {t(`dubai_p`)}
-                  </p>
-                  {/* <div className="d-flex justify-content-between">
-                                    <a className="btn btn-sm btn-primary rounded py-2 px-4" href="">View Detail</a>
-                                    <a className="btn btn-sm btn-dark rounded py-2 px-4" href="">Book Now</a>
-                                </div> */}
-                </div>
-              </div>
-            </div>
+            <Tour
+              tours={tours}
+              key={tours.key}
+            />
           </div>
         </div>
       </div>
---
+++ src/components/Explore/assets/Tour.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+// import ToursData from "./Tours.json";
+
+export default function Tour(props) {
+  const { t } = useTranslation();
+
+  const [tours, setTours] = useState(props.tours);
+
+  return (
+    <>
+      {tours.map((tour) => {
+        return (
+          <div
+            className="col-lg-4 col-md-6 wow fadeInUp"
+            data-wow-delay="0.1s"
+            key={tour.name}
+          >
+            <div className="room-item shadow rounded overflow-hidden">
+              <div className="position-relative">
+                <img
+                  className="img-fluid"
+                  src={`./${tour.img}`}
+                  alt=""
+                />
+                <small className="position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4">
+                  {tour.price}/{t(`day`)}
+                </small>
+              </div>
+              <div className="p-4 mt-2">
+                <div className="d-flex justify-content-between mb-3">
+                  <h5 className="mb-0">{t(`${tour.name}`)}</h5>
+                  <div className="ps-2">
+                    <small className="fa fa-star text-primary"></small>
+                    <small className="fa fa-star text-primary"></small>
+                    <small className="fa fa-star text-primary"></small>
+                    <small className="fa fa-star text-primary"></small>
+                    <small className="fa fa-star text-primary"></small>
+                  </div>
+                </div>
+                <div className="d-flex mb-3">
+                  <small className="border-end me-3 pe-3">
+                    <i className="fa fa-bed text-primary me-2"></i>
+                    {t(`room`)}
+                  </small>
+                  <small className="border-end me-3 pe-3">
+                    <i className="fa fa-utensils text-primary me-2"></i>
+                    {t(`food`)}
+                  </small>
+                  <small>
+                    <i className="fa fa-headset text-primary me-2"></i>
+                    {t(`gid`)}
+                  </small>
+                </div>
+                <p className="text-body mb-3">{t(`${tour.description}`)}</p>
+                <div className="d-flex justify-content-between">
+                  <a
+                    className="btn btn-sm btn-primary rounded py-2 px-4"
+                    href=""
+                  >
+                    {t(`buy`)}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
